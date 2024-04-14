dd2499414f688e0853e737096433219545c94090
Patricia Ferrer <124654139+PatsFerrer@users.noreply.github.com>
2024-04-14 18:09:36 -0300

    estruturas dos componentes

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,31 @@
+import { CustomerReviews, Footer, Hero, PopularProducts, Services, SpecialOffer, Subscribe, SuperQuality } from './sections/index'
+
 const App = () => (
   <main className="relative">
     {/* <Nav/> */}
-    Nav
     <section className="xl:padding-1 wide:padding-r padding-b">
-      Hero
+      <Hero />
     </section>
     <section className="padding">
-      PopularProducts
+      <PopularProducts />
     </section>
     <section className="padding">
-      superQuality
+      <SuperQuality />
     </section>
     <section className="padding-x py-10">
-      Services
+      <Services />
     </section>
     <section className="padding">
-      SpecialOffer
+      <SpecialOffer />
     </section>
     <section className="padding bg-pale-blue padding">
-      CostumerReviews
+      <CustomerReviews />
     </section>
     <section className="padding-x sm:py-32 py-16 w-full">
-      Subscribe
+      <Subscribe />
     </section>
     <section className="bg-black padding-x padding-t pb-8">
-      Footer
+      <Footer />
     </section>
 
   </main>
